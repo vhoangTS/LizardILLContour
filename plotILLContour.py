a13f4cd84c3e11a2c678dc64f3f2c8ae6794bb15
vhoang <levy.2807@gmail.com>
2018-02-14 12:04:36 +0100

    Making it sexier

--- plotILLContour.py
+++ plotILLContour.py
@@ -15,7 +15,7 @@ def getFiles(modelpath, variantname, airnodename):
     return illfile, ptsfile
 
 
-def readFiles(ptsfile, illfile):
+def readFiles(plotmode, ptsfile, illfile):
     Xmatrix, Ymatrix, Zmatrix = [], [], []
     ptsf = open(ptsfile, "r")
     ptslines = ptsf.readlines()
@@ -31,57 +31,105 @@ def readFiles(ptsfile, illfile):
     illlines = illf.readlines()
     DA300line = illlines[-4].split()
     DA300line.pop(0)
-    for item in DA300line:
-        Zmatrix.append(float(item))
+    DFline = illlines[-1].split()
+    DFline.pop(0)
+    if plotmode == "DA":
+        for item in DA300line:
+            Zmatrix.append(float(item))
+    elif plotmode == "DF":
+        for item in DFline:
+            Zmatrix.append(float(item))
     illf.close()
     return Xmatrix, Ymatrix, Zmatrix
 
 
+def plotting(plotmode, Xmatrix, Ymatrix, Zmatrix, vname):
+    if plotmode == "DF":
+        title = "Daylight Factor"
+        endscale = 6
+        stepscale = 1
+        color = "Jet"
+    elif plotmode == "DA":
+        title = "Daylight Autonomy"
+        endscale = 100
+        stepscale = 10
+        color = "Jet"
+    imgsiz = 1600
+    xdis = abs(max(Xmatrix) - min(Xmatrix))
+    ydis = abs(max(Ymatrix) - min(Ymatrix))
+    ratio = ydis / xdis
+    trace = go.Contour(
+            z=Zmatrix,
+            x=Xmatrix,
+            x0=Xmatrix[0],
+            dx=abs(Xmatrix[0]-Xmatrix[1]),
+            y=Ymatrix,
+            y0=Ymatrix[0],
+            dy=abs(Ymatrix[0]-Ymatrix[1]),
+            colorscale=color,
+            autocontour=False,
+            colorbar=dict(
+                    title=title,
+                    titleside='right',
+                    titlefont=dict(size=imgsiz/125),
+                    ),
+            contours=dict(
+                    coloring='heatmap',
+                    showlabels=True,
+                    labelfont=dict(size=imgsiz/125),
+                    start=0,
+                    end=endscale,
+                    size=stepscale,
+                    ),
+            line=dict(
+                color='rgb(0, 0, 0)',
+                smoothing=0.85,
+                dash='dot',
+                width=1),
+                    )
+    data = [trace]
+    layout = go.Layout(
+            title="%s - %s" % (title, vname),
+            xaxis=dict(
+                    autorange=True,
+                    showgrid=False,
+                    zeroline=False,
+                    showticklabels=False,
+                    tickmode="auto",
+                    tick0=0,
+                    dtick=abs(Ymatrix[0]-Ymatrix[1]),
+                    domain=[min(Xmatrix), max(Xmatrix)]
+                    ),
+            yaxis=dict(
+                    autorange=True,
+                    showgrid=False,
+                    zeroline=False,
+                    showticklabels=False,
+                    tickmode="auto",
+                    tick0=0,
+                    dtick=abs(Ymatrix[0]-Ymatrix[1]),
+                    domain=[min(Ymatrix), max(Ymatrix)]
+                    )
+                        )
+    fig = go.Figure(data=data, layout=layout)
+    py.offline.plot(
+        fig,
+        filename="%s_%s.html" % (vname, plotmode),
+        image='png',
+        image_filename="%s_%s" % (vname, plotmode),
+        image_width=imgsiz,
+        image_height=ratio * imgsiz,
+        )
+
+
 modelpath = "p:\\Milan_Focchi_Russoli_180104\\"\
             "Thermal\\20180205_Kickoff\\Model\\"
-variantname = "V3_ACT_SV1_SV2"
+variantname = ["V3_ACT_SV1_SV2"]
 airnodename = "OFF"
+plotmode = ["DF", "DA"]  # DF: Daylight Factor, DA: Daylight Autonomy
 
-# modelpath = "c:\\Users\\vhoang\\Desktop\\LizardILLContour\\Model\\"
-# variantname = "V0_DAYLIGHT"
-# airnodename = "OFFICE"
-
-illfile, ptsfile = getFiles(modelpath, variantname, airnodename)
-Xmatrix, Ymatrix, Zmatrix = readFiles(ptsfile, illfile)
-
-trace = go.Contour(
-        z=Zmatrix,
-        x=Xmatrix,
-        x0=Xmatrix[0],
-        dx=abs(Xmatrix[0]-Xmatrix[1]),
-        y=Ymatrix,
-        y0=Ymatrix[0],
-        dy=abs(Ymatrix[0]-Ymatrix[1]),
-        colorscale="Hot",
-        autocontour=False,
-        contours=dict(
-                showlabels=True,
-                start=0,
-                end=100,
-                size=10,
-                   )
-                   )
-data = [trace]
-layout = go.Layout(
-        xaxis=dict(
-                autorange=True,
-                tickmode="auto",
-                tick0=0,
-                dtick=abs(Ymatrix[0]-Ymatrix[1]),
-                domain=[min(Xmatrix), max(Xmatrix)]
-                ),
-        yaxis=dict(
-                autorange=True,
-                tickmode="auto",
-                tick0=0,
-                dtick=abs(Ymatrix[0]-Ymatrix[1]),
-                domain=[min(Ymatrix), max(Ymatrix)]
-                )
-                        )
-fig = go.Figure(data=data, layout=layout)
-py.offline.plot(fig, filename="test.html")
+for vname in variantname:
+    for plotm in plotmode:
+        illfile, ptsfile = getFiles(modelpath, vname, airnodename)
+        Xmatrix, Ymatrix, Zmatrix = readFiles(plotm, ptsfile, illfile)
+        plotting(plotm, Xmatrix, Ymatrix, Zmatrix, vname)
